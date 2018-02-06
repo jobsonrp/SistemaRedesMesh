5f3da4578978df8ad0b50f025c24c03d7ad63f33
jpereira <jobsonrp@gmail.com>
2018-02-06 13:45:38 -0300

    Implementada a consulta de Qtd de Macs por Canal.

--- index.php
+++ index.php
@@ -5,6 +5,9 @@
 	$qtdsP = array();
 	$potencias = array();
 	
+	$qtdsC = array();
+	$canais = array();
+	
 	$cor = array();
 	
 	$cor[0] = '#ff3300';
@@ -79,7 +82,12 @@
 	//$sql = "SELECT COUNT(rssi) as qtd FROM dadosTeste WHERE datetime BETWEEN $dt1_Str AND $dt2_Str";
 	$resultado = mysqli_query($conexao,$sql);
 	$row3 = mysqli_fetch_object($resultado);
-	$row2 = $row3 -> qtd;*/
+	$row2 = $row3 -> qtd;
+	
+	SELECT COUNT(ch) as qtd,ch FROM dadosTeste GROUP BY ch,addr
+	SELECT COUNT(ch) as qtd,ch FROM dadosMesh GROUP BY ch // contando os masc repetidos
+	*/
+	
 	
 	if(!isset($_GET['tipoBusca'])){
 		$tipoBusca= 'nenhuma';
@@ -132,11 +140,30 @@
 			$i = $i + 1;
 			$pot1 = $pot1 + $intervalo;
 			$pot2 = $pot2 + $intervalo;
-			
+		}
+	}
+	
+	elseif ($tipoBusca == 'Canal'){
+		$i = 0;
+		$dt1 = new DateTime($data1 . " " . $hora1);
+		$dt2 = new DateTime($data2 . " " . $hora2);
+		$dt1_Str = $dt1->format('Y-m-d H:i:s');
+		$dt2_Str = $dt2->format('Y-m-d H:i:s');
+		
+		$sql = "SELECT COUNT(ch) as qtd,ch FROM dadosMesh WHERE datetime BETWEEN '$dt1_Str' AND '$dt2_Str' GROUP BY ch";
+		//$sql = "SELECT COUNT(ch) as qtd,ch FROM dadosTeste GROUP BY ch";
+		$resultado = mysqli_query($conexao,$sql);
+		while ($row = mysqli_fetch_object($resultado)){
+			$qtd = $row -> qtd;
+			$canal = $row -> ch;
+			$qtdsC[$i] = $qtd;
+			$canais[$i] = $canal;
+			$i = $i + 1;			
 		}
 	}
 	
 ?>
+
 
 <!DOCTYPE html>
 
@@ -196,8 +223,7 @@
         <ul class="nav navbar-nav navbar-right">
             <li class="active"><a href="#top-section">Home</a></li>
             <li><a href="#Section-1">Gráficos</a></li>
-            <li><a href="#Section-4">Alunos</a></li>
-            
+            <li><a href="#Section-4">Desenvolvedores</a></li> 
         </ul>
     </div>
     <!-- /.navbar-collapse -->
@@ -214,7 +240,7 @@
                          <h1>Sistema Web - Redes Mesh .</h1>
                     </div>
                     <br/>
-                    <a href="#Section-1" class="buttonyellow animated fadeInLeftBig"><b>Gráfico 1</b></a>
+                    <a href="#Section-1" class="buttonyellow animated fadeInLeftBig"><b>Gráficos</b></a>
                 </div>
             </div>
             <div class="item" id="slide_2">
@@ -223,16 +249,16 @@
                          <h1>Disciplinas de Redes - RSI</h1>
                     </div>
                     <br/>
-                    <a href="#Section-4" class="buttoncolor animated fadeInRightBig"><b>Alunos</b></a>
+                    <a href="#Section-4" class="buttoncolor animated fadeInRightBig"><b>Desenvolvedores</b></a>
                 </div>
             </div>
             <div class="item" id="slide_3">
                 <div class="carousel-content">
                     <div class="animated fadeInDownBig">
-                         <h1>Redes Mesh ...</h1>
+                         <h1>Rede Mesh com NodeMCU</h1>
                     </div>
                         <br/>
-                        <a href="https://github.com/flushedlucas/MeshSniffer" class="buttonyellow animated fadeInLeftBig"><b>Projeto</b></a>
+                        <a href="https://github.com/flushedlucas/MeshSniffer" class="buttonyellow animated fadeInLeftBig"><b>Projeto (Github)</b></a>
                 </div>
             </div>
         </div>
@@ -263,6 +289,7 @@
 					<select class="form-control" id="tipoBusca" name="tipoBusca" value = <?php echo $tipoBusca ?>>
 						<option value="Data" <?php if ($tipoBusca == 'Data') echo 'selected="selected"'; ?> >Por Intervalo de Tempo</option>
 						<option value="Potencia" <?php if ($tipoBusca == 'Potencia') echo 'selected="selected"'; ?> >Por Faixa de Potência</option>
+						<option value="Canal" <?php if ($tipoBusca == 'Canal') echo 'selected="selected"'; ?> >Por Canal</option>
 					</select>
 				</div>
 				<div class="form-group">
@@ -280,14 +307,15 @@
 				
 				<table style="width:100%">
 				  <tr>
-				    <td>
-					<div class="form-group;">
-					<div id="intervaloTempo" <?php if ($tipoBusca == 'Potencia') echo 'hidden'; ?> >Intervalo de Tempo (min):</div>
-					<div id="faixaPotencia" <?php if ($tipoBusca == 'Data') echo 'hidden'; ?> >Faixa de Potência (dBm):</div>
-					<input class="form-control" id="intervalo" name="intervalo" type="number" value = <?php echo $intervalo ?> required/>
-				    </div>			    
-				    
-				    </td>
+				    <div id="valorBusca">
+					    <td>
+						<div class="form-group;">
+						<div id="intervaloTempo" <?php if ($tipoBusca == 'Potencia') echo 'hidden'; ?> >Intervalo de Tempo (min):</div>
+						<div id="faixaPotencia" <?php if ($tipoBusca == 'Data') echo 'hidden'; ?> >Faixa de Potência (dBm):</div>
+							<input class="form-control" id="intervalo" style="width:20%;" name="intervalo" type="number" value = <?php echo $intervalo ?> <?php if ($tipoBusca == 'Canal') echo 'hidden'; ?> required/>
+					    </div>			    
+					    </td>
+					</div>
 				    <td style="text-align:right;">
 				    <div class="form-group">
 					<input class="btn-success btn-lg align-bottom" type="submit" value="Plotar"></input>
@@ -300,7 +328,7 @@
 				
 	</form>
 		
-</br></br></br>
+</br>
 
 <div id="grafico1">
 
@@ -339,7 +367,7 @@
 
       var options = {
         title: "Quantidade de MAC's por Intervalo de Tempo",
-        width: 800,
+        width: '100%',
         height: 400,
         vAxis: {title: "Quantidade de MAC's capturados"},
         hAxis: {title: "Data"},
@@ -350,8 +378,7 @@
       chart.draw(view, options);
   }
   </script>
-  <div id="columnchart_values1" style="width: 900px; height: 300px;"></div>
-
+  <div id="columnchart_values1" style="width:100%; height: 300px;"></div>
 </div>
 
 <div id="grafico2">
@@ -391,7 +418,7 @@
 
       var options = {
         title: "Quantidade de MAC's por Faixa de Potência",
-        width: 800,
+        width: '100%',
         height: 400,
         vAxis: {title: "Quantidade de MAC's capturados"},
         hAxis: {title: "Potência"},
@@ -402,10 +429,61 @@
       chart.draw(view, options);
   }
   </script>
-  <div id="columnchart_values2" style="width: 900px; height: 300px;"></div>
+  <div id="columnchart_values2" style="width: 100%; height: 300px;"></div>
 
 </div>
 
+<div id="grafico3">
+
+<script type="text/javascript" src="site/loader.js"></script>
+  <script type="text/javascript">
+    google.charts.load("current", {packages:['corechart']});
+    google.charts.setOnLoadCallback(drawChart);
+    function drawChart() {
+      var data = google.visualization.arrayToDataTable([
+        ["Canal", "Qdt", { role: "style" } ],
+		<?php 
+		$c = 0;
+		$k = $i;
+		for ($i=0; $i < $k; $i++){
+		?>
+
+		['<?php echo $canais[$i] ?>', <?php echo $qtdsC[$i] ?>, '<?php echo $cor[$c] ?>'],
+
+		<?php 
+		$c = $c + 1;
+		if($c > 12){
+			$c = 0;
+			}
+			
+		} ?>
+        
+      ]);
+
+      var view = new google.visualization.DataView(data);
+      view.setColumns([0, 1,
+                       { calc: "stringify",
+                         sourceColumn: 1,
+                         type: "string",
+                         role: "annotation" },
+                       2]);
+
+      var options = {
+        title: "Quantidade de MAC's por Canal",
+        width: '100%',
+        height: 400,
+        vAxis: {title: "Quantidade de MAC's capturados"},
+        hAxis: {title: "Canal"},
+        bar: {groupWidth: "95%"},
+        legend: { position: "none" },
+      };
+      var chart = new google.visualization.ColumnChart(document.getElementById("columnchart_values3"));
+      chart.draw(view, options);
+  }
+  </script>
+  <div id="columnchart_values3" style="width: 100%; height: 300px;"></div>
+
+</div>
 
 </section>
 
@@ -416,7 +494,7 @@
 <div class="container">
 <div class="row">
     <div class="page-header text-center col-sm-12 col-lg-12 animated fade">
-        <h1>Alunos</h1>
+        <h1>Desenvolvedores</h1>
     </div>
 </div>
 <div class="row testimonials animated fadeInUpNow">
@@ -430,15 +508,18 @@
                 </div>
                 <div class="testimonials-carousel-context">
                     <div class="testimonials-name">
-                         Aluno 1 <span>redesmesh</span>
+                         Jobson Rocha <span>UFRPE - Aluno</span>
                     </div>
                     <div class="testimonials-carousel-content">
                         <p>
                              Telefone: (81) 91919-9191
                         </p>
                         <p>
-                             E-mail: aluno1@com
+                             E-mail: jobson@com
                         </p>
+                    <div class="testimonials-name">
+                         Professor: Glauco Gonçalves
+                    </div>
                     </div>
                 </div>
             </div>
@@ -448,7 +529,7 @@
                 </div>
                 <div class="testimonials-carousel-context">
                     <div class="testimonials-name">
-                         Aluno 2 <span>redesmesh</span>
+                         Lucas Vieira <span>UFRPE - Aluno</span>
                     </div>
                     <div class="testimonials-carousel-content">
                         <p>
@@ -456,8 +537,11 @@
                         </p>
                     </div>
                     <p>
-                         E-mail: aluno2@com
+                         E-mail: lucas@com
                     </p>
+                    <div class="testimonials-name">
+                         Professor: Glauco Gonçalves
+                    </div>
                 </div>
             </div>
             <div class="slide">
@@ -466,7 +550,7 @@
                 </div>
                 <div class="testimonials-carousel-context">
                     <div class="testimonials-name">
-                         Aluno 3 <span>redesmesh</span>
+                         Luiz Felipe <span>UFRPE - Aluno</span>
                     </div>
                     <div class="testimonials-carousel-content">
                         <p>
@@ -474,8 +558,11 @@
                         </p>
                     </div>
                     <p>
-                         E-mail: aluno3@com
+                         E-mail: luiz@com
                     </p>
+                    <div class="testimonials-name">
+                         Professor: Glauco Gonçalves
+                    </div>
                 </div>
             </div>
         </div>
@@ -536,11 +623,14 @@
 <script>
 $(document).ready(function(){
   $("#grafico1").hide();
+  $("#valorBusca").show();
     $('#tipoBusca').on('change', function() {
       if ( this.value == 'Data')
       {
         $("#grafico1").show();
         $("#intervaloTempo").show();
+        $("#valorBusca").show();
+        $("#intervalo").show();
       }
       else
       {
@@ -555,11 +645,14 @@ $(document).ready(function(){
 $(document).ready(function(){
   $("#grafico2").hide();
   $("#faixaPotencia").hide();
+  $("#valorBusca").show();
     $('#tipoBusca').on('change', function() {
       if ( this.value == 'Potencia')
       {
         $("#grafico2").show();
         $("#faixaPotencia").show();
+        $("#valorBusca").show();
+        $("#intervalo").show();
       }
       else
       {
@@ -571,18 +664,50 @@ $(document).ready(function(){
 </script>
 
 <script>
+$(document).ready(function(){
+  $("#grafico3").hide();
+  $("#valorBusca").hide();
+  $("#intervalo").hide();
+    $('#tipoBusca').on('change', function() {
+      if ( this.value == 'Canal')
+      {
+        $("#grafico3").show();
+      }
+      else
+      {
+        $("#grafico3").hide();
+        $("#valorBusca").show();
+        $("#intervalo").show();
+      }
+    });
+});
+</script>
+
+<script>
 function mostrarCampoBusca() {
     var t = document.getElementById('tipoBusca');
     var i = document.getElementById('intervaloTempo');
     var f = document.getElementById('faixaPotencia');
+    var c = document.getElementById('valorBusca');
+    var it = document.getElementById('intervalo');
 
     if (t.value === 'Data') {
         i.style.display = 'block';
         f.style.display = 'none';
+        c.style.display = 'none';
+        it.style.display = 'block';
     }
     else if (t.value === 'Potencia') {
     	i.style.display = 'none';
         f.style.display = 'block';
+        c.style.display = 'none';
+        it.style.display = 'block';
+    }
+    else if (t.value === 'Canal') {
+    	i.style.display = 'none';
+        f.style.display = 'none';
+        c.style.display = 'block';
+        it.style.display = 'none';
     }
 }
 </script>
@@ -591,6 +716,7 @@ function mostrarCampoBusca() {
 function mostrarGrafs() {
     var x = document.getElementById('grafico1');
     var y = document.getElementById('grafico2');
+    var w = document.getElementById('grafico3');
     var z = document.getElementById('tipoBusca');
     //var x = document.getElementById('infoPerfil');
     //z.selectedIndex = 1;
@@ -601,6 +727,10 @@ function mostrarGrafs() {
     else if (z.value === 'Potencia') {
         y.style.display = 'block';
         z.selectedIndex = 1;
+    }
+    else if (z.value === 'Canal') {
+        w.style.display = 'block';
+        z.selectedIndex = 2;
     }
 
 }
